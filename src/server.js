28f2e6d1e41881d227744ac49473eba5ecacb25b
GinKalp <80583818+GinKalp@users.noreply.github.com>
2021-10-25 08:54:27 +0300

    added routers

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path')
-
+const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
-const app = express();
+const accountsRouter = require('./routes/accountsRouter')
+const billsRouter = require('./routes/billsRouter')
+
+
 
 // set engine, default directory /views
 app.set('view engine', 'ejs')
@@ -15,6 +18,9 @@ app.set('views', 'src/views')
 app.use(morgan('common'));
 app.use(cors());
 app.use(express.json());
+
+app.use('/accounts', accountsRouter)
+app.use('/bills', billsRouter)
 
 // static directory for css, img, js front files
 const staticPath = path.join(__dirname, 'assets')
